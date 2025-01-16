a371f8812a3b5ce26d0d885a0119babd566655ff
fc510 <frostyblueye@gmail.com>
2025-01-16 17:58:02 +0800

    upd

--- run.sh
+++ run.sh
@@ -2,5 +2,21 @@
 
 . ./run.conf
 
-echo $ANSIBLE_VERSION | jq .[0]
-echo $GITLAB_EE_VERSION | jq .[0]
+function get_items(){
+    LIST="${1}"
+    # DELIM=','
+    DELIM="${2}"
+    echo -n ${LIST} | sed "s/\[//g" | sed "s/\]//g" | sed "s/\"//g" #| sed "s/${DELIM}/\n/g"
+}
+
+# get_items ${GITLAB_EE_VERSION} " "
+
+for i in ${GITLAB_EE_VERSION//,/ }; do 
+    echo $i;     
+done
+
+S1="https://packages.gitlab.com/gitlab/gitlab-ee/packages/el/8/gitlab-ee-${version}-ee.0.el8.x86_64.rpm/download.rpm"
+lastToken="${S1##*/}"
+echo "$lastToken"
+
+
